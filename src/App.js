5d87858ae5935d9bf57c89156d67d4d188953c55
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-10-11 18:16:16 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST
+          TEST v0.0.6
         </p>
         <a
           className="App-link"
